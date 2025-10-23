0027fd9cea7cfe26739dd871378061a5c34294fa
Luiz Gustavo Frota <luizgustavo.frota@technipfmc.com>
2025-10-23 00:45:54 -0300

    handles database ssl connection

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,13 +2,12 @@ import query from "infra/database";
 
 export default async function status(request, response) {
   const updatedAt = new Date().toISOString();
-
   const postgresVersionResult = await query("SHOW SERVER_VERSION;");
-  const postgresVersionValue = postgresVersionResult.rows[0].server_version;
+  const postgresVersionValue = postgresVersionResult?.rows[0]?.server_version;
 
   const postgresMaxConnectionResult = await query("SHOW max_connections;");
   const postgresMaxConnectionValue =
-    postgresMaxConnectionResult.rows[0].max_connections;
+    postgresMaxConnectionResult?.rows[0]?.max_connections;
 
   const databaseName = process.env.POSTGRES_DB;
   const postgresOpenedConnectionsResult = await query({
@@ -17,7 +16,7 @@ export default async function status(request, response) {
   });
 
   const postgresOpenedConnectionsValue =
-    postgresOpenedConnectionsResult.rows[0].count;
+    postgresOpenedConnectionsResult?.rows[0]?.count;
 
   response.status(200).json({
     updated_at: updatedAt,
