88a8a5fdc579fb15fdcdab8f70058bc880450bac
Luiz Gustavo Frota <luizgustavo.frota@technipfmc.com>
2025-11-18 00:07:43 -0300

    feat: create and use `migrator` model

---
+++ models/migrator.js
@@ -0,0 +1,64 @@
+import migrationRunner from "node-pg-migrate";
+import database from "infra/database";
+import { resolve } from "node:path";
+import { ServiceError } from "infra/errors";
+
+export const defaultMigrationOptions = {
+  dryRun: true,
+  dir: resolve("infra", "migrations"),
+  direction: "up",
+  verbose: true,
+  migrationsTable: "pgmigrations",
+};
+
+async function listPendingMigrations() {
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+
+    const pendingMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dbClient: dbClient,
+    });
+
+    return pendingMigrations;
+  } catch (error) {
+    const serviceErrorObject = new ServiceError({
+      message: "Erro ao listar migrações pendentes.",
+      cause: error,
+    });
+    throw serviceErrorObject;
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+async function runPendingMigrations() {
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dbClient: dbClient,
+      dryRun: false,
+    });
+
+    return migratedMigrations;
+  } catch (error) {
+    const serviceErrorObject = new ServiceError({
+      message: "Erro ao rodar migrações pendentes.",
+      cause: error,
+    });
+    throw serviceErrorObject;
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+const migrator = {
+  listPendingMigrations,
+  runPendingMigrations,
+};
+
+export default migrator;
