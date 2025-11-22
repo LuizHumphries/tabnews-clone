bebd4812d38ed5c75b451366d41b0fe26048cc4c
Luiz Gustavo Frota <luizgustavo.frota@technipfmc.com>
2025-11-22 17:16:55 -0300

    fix: adjust migration file for password, created_at and  updated_at fields

--- infra/migrations/1763496506814_create-users.js
+++ infra/migrations/1763496506814_create-users.js
@@ -23,20 +23,22 @@ exports.up = (pgm) => {
       unique: true,
     },
 
-    // Why 72 in length? https://security.stackexchange.com/a/39851
+    // Why 60 in length? https://www.npmjs.com/package/bcrypt#hash-info
     password: {
-      type: "varchar(72)",
+      type: "varchar(60)",
       notNull: true,
     },
 
     created_at: {
       type: "timestamptz",
-      default: pgm.func("now()"),
+      notNull: true,
+      default: pgm.func("timezone('utc', now())"),
     },
 
     updated_at: {
       type: "timestamptz",
-      default: pgm.func("now()"),
+      notNull: true,
+      default: pgm.func("timezone('utc', now())"),
     },
   });
 };
